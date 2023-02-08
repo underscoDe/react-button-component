e70fe9eb33c87fa9751ffc38e12c7c80056d7b7b
Arsene Awounou <developer.awounou@gmail.com>
2023-02-08 15:52:50 +0100

    task done

--- src/Button.js
+++ src/Button.js
@@ -3,8 +3,86 @@ import styled from "styled-components";
 
 import { COLORS } from "./constants";
 
-const Button = ({ variant, size, children }) => {
-  return "TODO";
+const STYLES = {
+  small: {
+    "--padding": "8px 16px",
+    "--borderRadius": 2 + "px",
+    "--fontSize": "1rem",
+  },
+  medium: {
+    "--padding": "16px 24px",
+    "--borderRadius": 2 + "px",
+    "--fontSize": 18 / 16 + "rem",
+  },
+  large: {
+    "--padding": "20px 36px",
+    "--borderRadius": 4 + "px",
+    "--fontSize": 21 / 16 + "rem",
+  },
 };
+
+const Button = ({ variant, size, children }) => {
+  const styles = STYLES[size];
+
+  const ButtonComponent =
+    variant === "fill"
+      ? FillButton
+      : variant === "outline"
+      ? OutlineButton
+      : GhostButton;
+
+  return <ButtonComponent style={styles}>Button</ButtonComponent>;
+};
+
+const ButtonBase = styled.button`
+  font-family: "Roboto", sans-serif;
+  font-size: var(--fontSize);
+  padding: var(--padding);
+  border-radius: var(--borderRadius);
+  border: 2px solid transparent;
+  color: ${COLORS.white};
+`;
+
+const FillButton = styled(ButtonBase)`
+  background: ${COLORS.primary};
+
+  &:focus {
+    outline-offset: -5px;
+    outline-color: ${COLORS.primary};
+  }
+
+  &:hover {
+    background: ${COLORS.primaryLight};
+  }
+`;
+
+const OutlineButton = styled(ButtonBase)`
+  background: ${COLORS.white};
+  color: ${COLORS.primary};
+  border-color: inherit;
+
+  &:focus {
+    outline-offset: -5px;
+  }
+
+  &:hover {
+    background: ${COLORS.offwhite};
+  }
+`;
+
+const GhostButton = styled(ButtonBase)`
+  background-color: transparent;
+  color: gray;
+
+  &:focus {
+    border-color: ${COLORS.transparentGray75};
+    border-radius: 8px;
+  }
+
+  &:hover {
+    background: ${COLORS.transparentGray15};
+    color: ${COLORS.black};
+  }
+`;
 
 export default Button;
